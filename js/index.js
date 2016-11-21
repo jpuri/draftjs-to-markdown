cfb0640fc3630ea23aafbeadde7eaf78cfe85cf4
jpuri <jyotipuri@gmail.com>
2016-11-21 16:39:59 +0530

    fixing if condition

--- js/index.js
+++ js/index.js
@@ -47,7 +47,7 @@ function getEntityMarkdown(entityMap: Object, entityKey: number, text: string): 
   if (entity.type === 'IMAGE') {
     return `!(${entity.data.src})`;
   }
-  if (entity.type === 'IMAGE') {
+  if (entity.type === 'EMBEDDED_LINK') {
     return `<iframe width="100%" height="315px" src="${entity.data.link}" frameBorder="0" allowFullScreen />`;
   }
   return text;
